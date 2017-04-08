5fe7f609bc5fb8ad8e6cceecb145d38692b8460b
s2haseeb <haseeb1496@gmail.com>
2017-04-07 23:59:31 -0400

    Copied generic .obj loader C++ functions

--- objloader.h
+++ objloader.h
@@ -1,4 +1,4 @@
-#ifndef OBJLOADER_H
+/*#ifndef OBJLOADER_H
 #define OBJLOADER_H
 
 
@@ -6,6 +6,17 @@ class objloader
 {
 public:
     objloader();
+
+
+    bool loadOBJ(
+        const char * path,
+        std::vector<glm::vec3> & out_vertices,
+        std::vector<glm::vec2> & out_uvs,
+        std::vector<glm::vec3> & out_normals
+        );
+
+#endif
 };
 
 #endif // OBJLOADER_H
+*/
