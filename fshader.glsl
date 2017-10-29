9bd1cbbe843aed48e10e519954f3870fef9e42a4
s2haseeb <haseeb1496@gmail.com>
2017-10-28 21:48:11 -0400

    Replaced shaders with Qt sample program shaders to actually see loaded
    object.

--- fshader.glsl
+++ fshader.glsl
@@ -1,5 +1,15 @@
-#version 330 core
-out vec3 color;
-void main(){
-  color = vec3(1,0,0);
+#ifdef GL_ES
+// Set default precision to medium
+precision mediump int;
+precision mediump float;
+#endif
+
+uniform sampler2D texture;
+
+varying vec2 v_texcoord;
+
+void main()
+{
+    // Set fragment color from texture
+    gl_FragColor = vec4(1, 0.5, 0, 0);
 }
--- vshader.glsl
+++ vshader.glsl
@@ -1,6 +1,22 @@
-#version 330 core
-layout(location = 0) in vec3 vertexPosition_modelspace;
-void main(){
-  gl_Position.xyz = vertexPosition_modelspace;
-  gl_Position.w = 1.0;
+#ifdef GL_ES
+// Set default precision to medium
+precision mediump int;
+precision mediump float;
+#endif
+
+uniform mat4 mvp_matrix;
+
+attribute vec4 a_position;
+attribute vec2 a_texcoord;
+
+varying vec2 v_texcoord;
+
+void main()
+{
+    // Calculate vertex position in screen space
+    gl_Position = mvp_matrix * a_position;
+
+    // Pass texture coordinate to fragment shader
+    // Value will be automatically interpolated to fragments inside polygon faces
+    v_texcoord = a_texcoord;
 }
